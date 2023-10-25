77eaa0bbdd67c4053ff4879da7ae8b25db90ae2e
joseantpul <114654377+joseantpul@users.noreply.github.com>
2023-10-25 18:12:57 +0100

    Update ej_11f.cs

--- ej_11f.cs
+++ ej_11f.cs
@@ -21,3 +21,15 @@ public class ej_11f : MonoBehaviour
       Debug.Log(other.gameObject.tag);
     }
 }
+
+//https://docs.unity3d.com/es/current/Manual/CollidersOverview.html
+/**
+Colisiones:
+No-trigger: al menos uno de los objetos debe ser no Kinematic
+Si ambos son Kinematic OnCollisionEnter no se llama
+En colisiones trigger la restricción anterior no se aplica.
+Static: Los objetos Rigidbody colisionan pero el static collider no se mueve. 
+No se debe alterar en tiempo de ejecución la propiedad
+Rigidbody Collider: Se mueve por física y reaccionan a colisiones y fuerzas.
+Kinematic Rigidbody: Se mueve con Transform liberándolo del motor de física.
+**/
